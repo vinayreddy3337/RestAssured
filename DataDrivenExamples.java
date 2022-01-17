7078fe2da16e08cb72a4c6807dadb3b3b01b45d7
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-01-18 04:11:36 +0530

    Add files via upload
    RESTAssured programs

---
+++ DataDrivenExamples.java
@@ -0,0 +1,19 @@
+package Rest;
+
+import org.testng.annotations.Test;
+
+public class DataDrivenExamples {
+@Test
+	public void test_post() {
+		
+	JSONObject request = new JSONObject();
+	
+	request.put("firstName","Tom");
+	request.put("LastName","Cooper");
+	request.put("")
+	
+	
+	}
+	
+	
+}
