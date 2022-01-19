a1b9c1a5212a2747be83755609116d1355c38414
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-01-20 05:07:57 +0530

    Create Samallest.java

---
+++ JAVA/Array/Samallest.java
@@ -0,0 +1,16 @@
+public class Vinay12
+{
+int i;
+  int m=0;
+		int array[]= {10,20,30,60,40};
+		min=array[0];
+		for(i=0;i<array.length;i++)
+		{
+			if(array[i]<min)
+				min=array[i];
+		}
+		System.out.print("minimum:" +min);
+	}
+
+}
+}
