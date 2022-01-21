12c3ca5905b94b1a0a50d8d7d14cae1ccfc2c35e
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-01-21 23:07:31 +0530

    Create smallest.java

---
+++ JAVA/Array/src/smallest.java
@@ -0,0 +1,17 @@
+public class Vinay12
+{
+	public static void main(String[] args) {
+int i;
+  int m=0;
+		int array[]= {10,20,30,60,40};
+		min=array[0];
+		for(i=0;i<array.length;i++)
+		{
+			if(array[i]<min)
+				min=array[i];
+		}
+		System.out.print("minimum:" +min);
+	}
+
+}
+}
