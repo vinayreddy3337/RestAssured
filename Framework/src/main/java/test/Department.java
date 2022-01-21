79f1dbd412e006be99747c578db181ba1038f3d4
vinayreddy3337 <97912978+vinayreddy3337@users.noreply.github.com>
2022-01-21 22:52:54 +0530

    Update Department.java

--- Framework/src/main/java/test/Department.java
+++ Framework/src/main/java/test/Department.java
@@ -1,31 +1,38 @@
 package org.restassureddemo.test;
 
-public class Department {
+public class Department
+{
 
     String id;
     String name;
 
-    public String getId() {
+    public String getId() 
+    {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(String id)
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public String getDescription() {
+    public String getDescription() 
+    {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description)
+    {
         this.description = description;
     }
 
